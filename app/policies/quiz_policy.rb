86115ee1ba0785621e4895956b46baf427d7500a
the-S-U-I-T <97888087+the-S-U-I-T@users.noreply.github.com>
2025-08-08 10:20:29 +0000

    Final_day_commit_with_Quizz_index_error

---
+++ app/policies/quiz_policy.rb
@@ -0,0 +1,16 @@
+class ProductPolicy
+    attr_reader :user, :quiz
+
+    def initialize(user, quiz)
+        @user = user
+        @uiz = quiz
+    end
+
+    def update?
+        user.admin? || !quiz.published?
+    end
+
+    def index?
+        @user.admin?
+    end
+end
